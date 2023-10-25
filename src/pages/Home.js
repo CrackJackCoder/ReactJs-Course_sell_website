9f3b2f8ee21e6b3f11460242f1d276b6c4e5414f
Harsh Kumar Singh <harshkumar23x@gmail.com>
2023-10-25 14:28:10 +0530

    fix filter capsule(vh)2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -48,26 +48,14 @@ function Home({isLogin, notification, setNotification,setCart,cart}) {
     }
     getCourses();
     return (
-        <div className=" bg-white text-3xl min-h-full pt-20">
-            <div className="flex justify-center gap-3 max-md:hidden">
+        <div className="relative bg-white text-3xl min-h-full pt-20">
+            <div className="flex justify-center gap-3 flex-wrap">
                 {
                     filterData.map((data) => {
                         return <FilterTab setfilterDatavar={setfilterDatavar} filterDatavar={filterDatavar} key={data.id} data={data} />
                     })
                 }
             </div>
-
-            <div className="flex justify-center mb-5">
-                <select className="absolute outline-none border border-richblack-200 text-center text-sm font-medium hidden max-md:block px-5 py-0">
-                    {
-                    filterData.map((data) => {
-                        return <option className="text-sm px-0 py-0"><FilterTab setfilterDatavar={setfilterDatavar} filterDatavar={filterDatavar} key={data.id} data={data} /></option>  
-                    })
-                    }
-                </select>
-            </div>
-
-            
 
 
             <div className="px-5 py-10 min-h-[85vh] flex gap-[50px] justify-center items-center flex-wrap">
